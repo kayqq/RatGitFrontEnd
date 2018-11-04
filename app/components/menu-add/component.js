4f2c2b9a5dc6d0ac64b2521079585c18db6b0574
Kenny Quan <quan_kenny@yahoo.com>
2018-11-03 19:47:49 -0700

    added menu-add component

--- app/components/menu-add/component.js
+++ app/components/menu-add/component.js
@@ -4,11 +4,9 @@ export default Component.extend({
     isExpanded: false,
     actions: {
         expandItemAdd() {
-            console.log('expanding');
             this.set('isExpanded', true);
         },
         contractItemAdd() {
-            console.log('contracting');
             this.set('isExpanded', false)
         }
     }
--- tests/integration/components/menu-add/component-test.js
+++ tests/integration/components/menu-add/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | menu-item-add', function(hooks) {
+module('Integration | Component | menu-add', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{menu-item-add}}`);
+    await render(hbs`{{menu-add}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#menu-item-add}}
+      {{#menu-add}}
         template block text
-      {{/menu-item-add}}
+      {{/menu-add}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
